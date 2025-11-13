99c35289133c1924e33416c2f428070938c8a55c
jrivilla <jrivillarepository@gmail.com>
2025-11-13 14:12:39 +0100

    feat: integrate better-auth for authentication and add user, session, account, and verification tables to the database schema feat: create auth client and implement sign-up functionality in the home page fix: update package.json and package-lock.json to include better-auth and its dependencies

--- meetai/src/app/page.tsx
+++ meetai/src/app/page.tsx
@@ -1,5 +1,72 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client"; //import the auth client
 
 export default function Home() {
-  return <Button>Click Me</Button>;
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch, //refetch the session
+  } = authClient.useSession();
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+        },
+        onSuccess: (ctx) => {
+          window.alert("Please check your email to verify your account.");
+          //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+          window.alert(ctx.error.message);
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        Welcome, {session.user?.name}
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>Sign Up</Button>
+      </div>
+    </div>
+  );
 }
